194456c921757d15c7e9a9e04ee725b19f383ad0
Ashish-Ashutosh <ashish.a.may@gmail.com>
2020-06-20 02:05:34 +0200

    added documentation for clarity.

--- src/App.js
+++ src/App.js
@@ -23,8 +23,9 @@ class App extends Component {
   deleteCharHandler = (index) => {
         // the below statement will convert a string into an array
         const text = this.state.userInput.split('');
+        //extracting the character that was clicked
         text.splice(index,1);
-        //the below statement will convert the array back to string
+        //the below statement will convert the array back to string whilst copying the remaining characters
         const updatedText = text.join('');
         this.setState({
             userInput:updatedText
@@ -34,18 +35,20 @@ class App extends Component {
 
   render() {
 
-    const charList = this.state.userInput.split('').map(ch, index => {
+    //extracting each element from the user input and sending it as "prop" to the Char Component
+    //Also, displaying the process via "charList" in return statement.
+    const charList = this.state.userInput.split('').map((ch, index)=> {
         return <CharComponent character={ch} key = {index}  clicked={() => this.deleteCharHandler(index)}/>
 
-    })
+    });
     return (
       <div className="App">
           <h2>
               <input type={"text"} onChange={this.inputChangeHandler} value={this.state.userInput}/>
               <p>{this.state.userInput}</p>
               <ValidationComponent inputLength ={this.state.length}/>
+              {/*Using a defined constant to be displayed as Output*/}
               {charList}
-              {/*<CharComponent boxit = {this.state.userInput}/>*/}
           </h2>
       </div>
     );
